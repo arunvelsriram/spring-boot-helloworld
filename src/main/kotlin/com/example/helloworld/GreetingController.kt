e68ddbf3d30691657420323c969fffafa567e12c
Arunvel Sriram <arunvelsriram@gmail.com>
2019-06-23 17:30:15 +0530

    Add log using slf4j and logback

--- src/main/kotlin/com/example/helloworld/GreetingController.kt
+++ src/main/kotlin/com/example/helloworld/GreetingController.kt
@@ -1,5 +1,6 @@
 package com.example.helloworld
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -11,7 +12,13 @@ data class Greeting(val id: Long, val content: String)
 class GreetingController {
     val counter = AtomicLong()
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(GreetingController::class.java)
+    }
+
     @GetMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-            Greeting(counter.incrementAndGet(), "Hello, $name")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
+        logger.info("Received name: $name")
+        return Greeting(counter.incrementAndGet(), "Hello, $name")
+    }
 }
